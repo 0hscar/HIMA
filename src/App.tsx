0068da0b73b5a4cce9efee2d535992a9c9d76bc2
0hscar <oscar.weber00@gmail.com>
2024-03-06 19:11:57 +0200

    structure changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Testing testing TJONG</Text>
+      <Button
+        title='Tjong'
+      ></Button>
       <StatusBar style="auto" />
     </View>
   );
